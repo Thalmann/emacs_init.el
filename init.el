7fd2e742bea6067d43b9fc8fdfac77b68c1c76dc
Bruno Thalmann <mr.thalmann@gmail.com>
2017-03-13 16:01:38 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(require 'cl)
+;; Start in fullscreen:
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 ;; If multiple dirs are recursively only prompt once not once for each dir
 (setq dired-recursive-deletes 'always)
 
@@ -30,6 +34,9 @@
 ;; Start RSS reader
 (global-set-key (kbd "<f9>") 'newsticker-show-news)
 
+;;Paranthesis highlighting
+(show-paren-mode t)
+(setq show-paren-style 'expression)
 
 ;; Spelling ISPELL
 ;; REQUIRED: Aspell installed from package manager
@@ -48,6 +55,18 @@
     
 (global-set-key (kbd "<f7>")   'fd-switch-dictionary)
 
+;; web-mode.el
+;; web-mode.org useful for editing html that use html engines
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; Eshell
 (defun eshell-clear-buffer ()
@@ -71,6 +90,9 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (deeper-blue)))
+ '(display-time-24hr-format t)
+ '(display-time-day-and-date t)
+ '(display-time-mode t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(newsticker-url-list
@@ -92,3 +114,110 @@
  )
 (put 'narrow-to-region 'disabled nil)
 (put 'erase-buffer 'disabled nil)
+
+
+;; Backup files
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+;; Enable encrypt files with gpg/gnupg
+(require 'epa-file)
+(epa-file-enable)
+
+;; Email client setup
+(require 'mu4e)
+
+;; default
+(setq mu4e-maildir (expand-file-name "~/Maildir"))
+
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+;; don't save message to Sent Messages, GMail/IMAP will take care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; setup some handy shortcuts
+(setq mu4e-maildir-shortcuts
+      '(("/INBOX"             . ?i)
+        ("/[Gmail].Sent Mail" . ?s)
+        ("/[Gmail].Trash"     . ?t)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+;; something about ourselves
+;; I don't use a signature...
+(setq
+ user-mail-address "bt@intempus.dk"
+ user-full-name  "Bruno Thalmann"
+ ;; message-signature
+ ;;  (concat
+ ;;    "Foo X. Bar\n"
+ ;;    "http://www.example.com\n")
+)
+
+;; sending mail -- replace USERNAME with your gmail username
+;; also, make sure the gnutls command line utils are installed
+;; package 'gnutls-bin' in Debian/Ubuntu, 'gnutls' in Archlinux.
+
+(require 'smtpmail)
+
+(setq message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials
+      '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials
+      (expand-file-name "~/.authinfo.gpg")
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-debug-info t)
+
+;; mu4e customs
+;; Open mail global key
+(global-set-key (kbd "C-x M") 'mu4e)
+
+;; When openeing an attachment based on the fileextension i provide
+;; some suggestions for directories to save them in:
+(setq mu4e-attachment-dir
+  (lambda (fname mtype)
+    (cond
+     ;; .txt files often are review files:
+     ((and fname (string-match "\\.txt$" fname))  "~/reviews")
+     (t "~/Downloads")))) ;; everything else
+
+;; Save attachment and then open it
+;; (global-set-key (kbd "C-c s r") (lambda () (interactive) (mu4e-view-save-attachment) (mu4e-view-attachment-action)))
+;; (eval-after-load 'mu4e '(define-key kbd "C-c s r" (lambda () (interactive) (mu4e-view-save-attachment) (mu4e-view-attachment-action))))
+
+;; jira setup
+;; (setq jiralib-url "https://intempus.atlassian.net")
+
+;; Org mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+;; Enable closed tag on todo tasks
+(setq org-log-done 'time)
+;; Enable closing note on todo tasks
+(setq org-log-done 'note)
+;; Set where the global todo list should look for todos:
+(setq org-agenda-files (list "~/org-files/"))
+
+;; Finances
+;; Set ledger in org-babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((ledger . t)))
+
+;; Time display format
+;; Set system load average to nil
+(setq display-time-default-load-average nil)
+
