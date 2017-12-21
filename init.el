9e3bdad036a5431c0a595ebef53cb66007f19cbc
Bruno Thalmann <mr.thalmann@gmail.com>
2017-12-21 12:04:13 +0100

    Added no more tabs and more concise magit log

--- init.el
+++ init.el
@@ -221,3 +221,35 @@
 ;; Set system load average to nil
 (setq display-time-default-load-average nil)
 
+;; No more tabs!!! Disable tabs
+(setq-default indent-tabs-mode nil)
+
+(use-package magit-log
+  :init
+  (progn
+    ;; (setq magit-log-margin '(t age magit-log-margin-width t 18)) ;Default value
+    (setq magit-log-margin '(t age-abbreviated magit-log-margin-width :author 11)))
+  :config
+  (progn
+    ;; Abbreviate author name. I added this so that I can view Magit log without
+    ;; too much commit message truncation even on narrow screens (like on phone).
+    (defun modi/magit-log--abbreviate-author (&rest args)
+      "The first arg is AUTHOR, abbreviate it.
+First Last  -> F Last
+First.Last  -> F Last
+Last, First -> F Last
+First       -> First (no change).
+
+It is assumed that the author has only one or two names."
+      ;; ARGS             -> '((AUTHOR DATE))
+      ;; (car ARGS)       -> '(AUTHOR DATE)
+      ;; (car (car ARGS)) -> AUTHOR
+      (let* ((author (car (car args)))
+             (author-abbr (if (string-match-p "," author)
+                              ;; Last, First -> F Last
+                              (replace-regexp-in-string "\\(.*?\\), *\\(.\\).*" "\\2 \\1" author)
+                            ;; First Last -> F Last
+                            (replace-regexp-in-string "\\(.\\).*?[. ]+\\(.*\\)" "\\1 \\2" author))))
+        (setf (car (car args)) author-abbr))
+      (car args))                       ;'(AUTHOR-ABBR DATE)
+    (advice-add 'magit-log-format-margin :filter-args #'modi/magit-log--abbreviate-author)))
